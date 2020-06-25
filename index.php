37926230009ae5f1bc4a861154fcbcb8a69d6138
akashrajput25 <55013687+akashrajput25@users.noreply.github.com>
2020-06-26 02:47:46 +0530

    updated frontend

--- index.php
+++ index.php
@@ -1,30 +1,69 @@
+
+<?php
+  $insert=false;
+  if(isset($_POST['name']))
+  {
+    $server ="localhost";
+    $username= "root";
+    $password ="";
+
+    $con = mysqli_connect($server,$username,$password);
+
+    if(!$con){
+        die("connection to this database failed ,reason".mysqli_connect_error());
+    }
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $age=$_POST['age'];
+    $gender=$_POST['gender'];
+    $phone=$_POST['phone'];
+    $desc=$_POST['desc'];
+
+    $sql= "INSERT INTO `trip`.`trip` ( `name`, 
+    `age`, `gender`, `email`, `phone`,
+    `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email',
+    '$phone', '$desc', current_timestamp())";
+
+    if($con ->query(($sql))== true)
+    {
+     $insert=true;
+    }
+    else
+    {
+     echo "Error :". $sql ."<br>". $con ->error;
+    }
+    $con->close();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php test</title>
+    <title>Travel Form</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <img class="bg" src="soa.jpg" alt="soa">
     <div class="container">
-      First php website
-      <?php
-      define('pie',3,14);
-       echo "hello bro";
-        $var1 =40;
-        $var2=60;
-        $var1=$var2;
-        echo "value of var1 and va2 is:"; echo $var1 + $var2;
-        echo "value of var1==var2 is:";
-        echo var_dump(1==2);
-        echo "<br>";
-        $var1++;
-        echo $var1;
-        echo "<br>";
-        echo var_dump($var1);
-        echo "<br>"; echo var_dump("hello man how are you");
-
+     <h3>Welcome to ITER Travel form</h3>   
+     <p>Enter your details to confirm this trip</p>
+     <?php
+     if($insert==true)
+     {
+     echo "<p class='asubmit'> See you near ITER bus stand ,have a nice day.</p>";
+     }
      ?>
+     <form action="index.php" method="post">
+         <input type ="text" name="name" id="name" placeholder="Enter your name">
+         <input type ="text" name="age" id="age" placeholder="Enter your age">
+         <input type ="text" name="gender" id="gender" placeholder="Enter your gender(M/F)">
+         <input type="email" name="email" id="email" placeholder="Enter your Email">
+         <input type="phone" name="phone" id="phone" placeholder="Enter you mobile no."> 
+         <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter other information"></textarea>
+         <button class="btn">Submit</button>     
+        </form>
     </div>
 </body>
 </html>
